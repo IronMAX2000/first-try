b6bc41f3d0b0ff9ca2e0899b32a9da195cda2362
IronMAX2000 <43476277+IronMAX2000@users.noreply.github.com>
2018-09-22 10:57:43 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,4 +2,29 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('я макс');
 $app->initLayout('Centered');
-$button = $app->add(['Button','maks ja tebja lublu']);
+$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=for_colibri;host=localhost','root','');
+
+class Client extends \atk4\data\Model {
+    public $table = 'client';
+function init(); {
+    parent::init();
+    $this->addField('name');
+    $this->addField('surname');
+    $this->addField('login');
+    $this->addField('password',['type'=>'password']);
+    $this->hasMany('friend', new Friend);
+}
+}
+
+class Friend extends \atk4\data\Model {
+    public $table = 'friend';
+function init(); {
+    parent::init();
+    $this->addField('name');
+    $this->addField('surname');
+    $this->addField('date',['type'=>'date']);
+    $this->addField('dept');
+    $this->hasOne('client_id', new Client)->addTitle();
+}
+}
